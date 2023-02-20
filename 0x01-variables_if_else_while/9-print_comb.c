06ecba9e2a8649d02d4520c56e2a3190907c1da2
EsliNsengiyumva <eslinsengiyumva@gmail.com>
2023-02-20 15:35:11 -0800

    Print out the list of number separated by comma and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *
+ * Display numbers from 0 to 9 with separation of comma and space
+ * 
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
