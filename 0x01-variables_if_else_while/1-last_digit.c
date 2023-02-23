f1987d9749826084050e6f04a6f64291eb3ae7fa
EsliNsengiyumva <eslinsengiyumva@gmail.com>
2023-02-23 14:51:43 -0800

    updated file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,28 +3,21 @@
 #include <stdio.h>
 
 /**
+ *main - Entry point
  *
- * The output of this program will be checking the last digit of the number by using modulo
- *
+ *Return: Always 0 (Success/correct)
  */
-
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n % 10 == 0)
-	{
-		printf("The last digit of %d is %d and is 0\n", n, n % 10);
-	}
-	else if (n % 10 < 6)
-	{
-		printf("The last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
-	}
-	else if (n % 10 > 5)
-	{
-		printf("The last digit of %d is %d and is greater than 5\n", n, n % 10);
-	}
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+printf("Last digit of %d is %d %s\n", n, n % 10,
+((n % 10) == 0) ? "and is 0"
+: (((n % 10) > 5) ? "and is greater than 5"
+: "and is less than 6 and not 0"));
+
+return (0);
 }
