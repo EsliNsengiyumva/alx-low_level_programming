d2490a595387ea55784adec9965b2d9f8dfeed1a
EsliNsengiyumva <eslinsengiyumva@gmail.com>
2023-02-24 02:52:35 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,8 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * _isupper - checks for uppercase character
+ * @letter: the character to be tracked
+ * Return: 1 if c is uppercase, 0 otherwise
  */
  int _isupper(int letter)
 {
