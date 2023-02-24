75d6422ec00ce2e628a7278f46884e10a1600f08
EsliNsengiyumva <eslinsengiyumva@gmail.com>
2023-02-24 00:16:42 -0800

    give you only one main file to test wit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
@@ -10,3 +10,4 @@ int main(void){
    return (0);
 
 }
+
