d246bfcea0ef99697d496268c6f98b33bc2c1abe
EsliNsengiyumva <eslinsengiyumva@gmail.com>
2023-02-25 15:18:34 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 /**
+ * main - Entry point
  *
- * Print numbers of base 16
- *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char j;
