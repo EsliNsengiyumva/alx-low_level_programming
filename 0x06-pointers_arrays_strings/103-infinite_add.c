b8d6f210a189845a8f047b4d4d1c149f7202b433
EsliNsengiyumva <eslinsengiyumva@gmail.com>
2023-03-02 13:22:48 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -15,31 +15,31 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	int begin = 0;
 	int swap = 0;
 
-	while (n1[i] != 0)/* A */
+	while (n1[i] != 0)
 		i++;
 	while (n2[j] != 0)
 		j++;
-	i--;/* C */
+	i--;
 	j--;
-	if (i > size_r || j > size_r)/* D */
+	if (i > size_r || j > size_r)
 		return (0);
-	for ( ; k < size_r; i--, j--, k++)/* E */
+	for ( ; k < size_r; i--, j--, k++)
 	{
 		sum = tens;
-		if (i >= 0)/* F */
+		if (i >= 0)
 			sum += n1[i] - '0';
 		if (j >= 0)
 			sum += n2[j] - '0';
-		if (i < 0 && j < 0 && sum == 0)/* G */
+		if (i < 0 && j < 0 && sum == 0)
 			break;
-		tens = sum / 10;/* H */
+		tens = sum / 10;
 		r[k] = sum % 10 + '0';
 	}
-	if (i >= 0 || j >= 0 || sum > 0)/* J */
+	if (i >= 0 || j >= 0 || sum > 0)
 		return (0);
-	r[k] = '\0';/* K */
+	r[k] = '\0';
 	k--;
-	for ( ; begin < k; k--, begin++)/* I */
+	for ( ; begin < k; k--, begin++)
 	{
 		swap = r[k];
 		r[k] = r[begin];
