dc3364ddfd6d70f5f86c8bf7be4ab630ecefcc63
EsliNsengiyumva <eslinsengiyumva@gmail.com>
2023-03-14 04:01:36 -0700

    function that concatenates all the arguments of your program

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -3,22 +3,63 @@
 #include<stdlib.h>
 
 /**
- * _strcat - Concat string, src appended to dest
+ * _strlen - returns the length of a string.
+ * @s: string whose length should be return
  *
- * @dest: Destination string
- * @src: Source string
- * Return: Concat newString to dest
+ * Return: (int) the length of the string.
  */
-char *_strcat(char *dest, char *src)
+int _strlen(char *s)
 {
-        int a, b;
-
-        for (a = 0; dest[a] != '\0'; a++)
-                ;
-        for (b = 0; src[b] != '\0'; b++)
-        {
-                dest[a] = src[b];
-                a++;
-        }
-        return (dest);
+	int i = 0;
+ 
+	while (*s)
+	{
+		i++;
+		s++;
+	}
+ 
+	return (i);
+}
+ 
+/**
+ * argstostr - concatenates all the arguments
+ * of a program.
+ * @ac: number of argument.
+ * @av: array of argument.
+ *
+ * Return: the pointer to the string or null.
+ */
+char *argstostr(int ac, char **av)
+{
+	int len = 0, i = 0, j, k = 0;
+	char *new_str;
+ 
+	if (ac <= 0 || !av)
+		return (NULL);
+ 
+	while (i < ac)
+	{
+		len += _strlen(av[i]);
+		i++;
+	}
+ 
+	new_str = malloc(sizeof(char) * (len + ac + 1));
+ 
+	if (!new_str)
+		return (new_str);
+	i = 0;
+	while (av[i])
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			new_str[k] = av[i][j];
+			k++;
+			j++;
+		}
+		new_str[k] = '\n';
+		k++;
+		i++;
+	}
+	return (new_str);
 }
