d534f4b76ac41233e3ac32cfe33f98de974cff82
EsliNsengiyumva <eslinsengiyumva@gmail.com>
2023-03-14 15:18:17 -0700

    function that concatenates all the arguments of your program.

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,65 +1,45 @@
 #include "main.h"
-#include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
 /**
- * _strlen - returns the length of a string.
- * @s: string whose length should be return
+ * argstostr - Concatenates all arguments of the program into a string;
+ *             arguments are separated by a new line in the string.
+ * @ac: The number of arguments passed to the program.
+ * @av: An array of pointers to the arguments.
  *
- * Return: (int) the length of the string.
- */
-int _strlen(char *s)
-{
-	int i = 0;
- 
-	while (*s)
-	{
-		i++;
-		s++;
-	}
- 
-	return (i);
-}
- 
-/**
- * argstostr - concatenates all the arguments
- * of a program.
- * @ac: number of argument.
- * @av: array of argument.
- *
- * Return: the pointer to the string or null.
+ * Return: If ac == 0, av == NULL, or the function fails - NULL.
+ *         Otherwise - a pointer to the new string.
  */
 char *argstostr(int ac, char **av)
 {
-	int len = 0, i = 0, j, k = 0;
-	char *new_str;
- 
-	if (ac <= 0 || !av)
+	char *str;
+	int arg, byte, index, size = ac;
+
+	if (ac == 0 || av == NULL)
 		return (NULL);
- 
-	while (i < ac)
+
+	for (arg = 0; arg < ac; arg++)
 	{
-		len += _strlen(av[i]);
-		i++;
+		for (byte = 0; av[arg][byte]; byte++)
+			size++;
 	}
- 
-	new_str = malloc(sizeof(char) * (len + ac + 1));
- 
-	if (!new_str)
-		return (new_str);
-	i = 0;
-	while (av[i])
+
+	str = malloc(sizeof(char) * size + 1);
+
+	if (str == NULL)
+		return (NULL);
+
+	index = 0;
+
+	for (arg = 0; arg < ac; arg++)
 	{
-		j = 0;
-		while (av[i][j])
-		{
-			new_str[k] = av[i][j];
-			k++;
-			j++;
-		}
-		new_str[k] = '\n';
-		k++;
-		i++;
+		for (byte = 0; av[arg][byte]; byte++)
+			str[index++] = av[arg][byte];
+
+		str[index++] = '\n';
 	}
-	return (new_str);
+
+	str[size] = '\0';
+
+	return (str);
 }
