1c18fdb5dd9bb227346f532068975b2cfafbc2c6
EsliNsengiyumva <eslinsengiyumva@gmail.com>
2023-04-09 03:56:20 -0700

    0x15. C - File I/O

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * read_textfile - read a textfile.
+ * @filename: name of the file to open.
+ * @letters: number of letters to print in the standar ouput.
+ *
+ * Return: number of letter printed.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd_open, fd_read, count_char;
+	char *bf_letters;
+
+	if (filename == NULL)
+		return (0);
+	fd_open = open(filename, O_RDONLY);
+	if (fd_open == -1)
+		return (0);
+	bf_letters = malloc(sizeof(char) * letters);
+	if (bf_letters == NULL)
+		return (0);
+	fd_read = read(fd_open, bf_letters, letters);
+	if (fd_read == -1)
+	{
+		free(bf_letters);
+		return (0);
+	}
+	count_char = write(STDOUT_FILENO, bf_letters, fd_read);
+	if (count_char == -1)
+	{
+		free(bf_letters);
+		return (0);
+	}
+	close(fd_open);
+	free(bf_letters);
+	return (count_char);
+}
+
