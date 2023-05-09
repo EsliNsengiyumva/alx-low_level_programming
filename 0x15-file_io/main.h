73d0e945716e409c503760550af2a2bc56f8100f
EsliNsengiyumva <eslinsengiyumva@gmail.com>
2023-05-09 08:07:23 -0700

    0x15. C - File I/O

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,22 +10,22 @@
 #include <elf.h>
 #define BUF_SIZE 1024
 
-int _putchar(char c);
-ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
-int _strlen(char *str);
-void close_file(int FD);
-void check_elf(unsigned char *e_ident);
-void print_magic(unsigned char *e_ident);
-void print_class(unsigned char *e_ident);
-void print_data(unsigned char *e_ident);
-void print_version(unsigned char *e_ident);
-void print_abi(unsigned char *e_ident);
-void print_osabi(unsigned char *e_ident);
-void print_type(unsigned int e_type, unsigned char *e_ident);
-void print_entry(unsigned long int e_entry, unsigned char *e_ident);
-void close_elf(int elf);
+int _putchar(char);
+ssize_t read_textfile(const char *, size_t);
+int create_file(const char *, char *);
+int append_text_to_file(const char *, char *);
+int _strlen(char *);
+void close_file(int);
+void check_elf(unsigned char *);
+void print_magic(unsigned char *);
+void print_class(unsigned char *);
+void print_data(unsigned char *);
+void print_version(unsigned char *);
+void print_abi(unsigned char *);
+void print_osabi(unsigned char *);
+void print_type(unsigned int, unsigned char *);
+void print_entry(unsigned long int, unsigned char *);
+void close_elf(int);
 
 
 
